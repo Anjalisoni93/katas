7a9ec34d0dace76e157a0773c3bfdee6e0f969c6
Anjali Soni <anjalipatadiya@gmail.com>
2021-10-18 22:16:50 +0000

    Vowels

---
+++ vowels.js
@@ -0,0 +1,15 @@
+const numberOfVowels = function(data) {
+  let vowels = 0;
+  var string = data.toString();
+
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i' || string.charAt(i) == 'o' || string.charAt(i) == 'u') {
+      vowels += 1;
+    }
+  }
+  return vowels;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
