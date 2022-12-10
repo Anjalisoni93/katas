cc4f05a2fa8faa1dffc3888319edca71d22f20eb
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-10 15:14:25 -0700

    Add a diffrent logic

--- vowels.js
+++ vowels.js
@@ -1,13 +1,21 @@
 const numberOfVowels = function(data) {
-  let vowels = 0;
-  var string = data.toString();
+  // let vowels = 0;
+  // var string = data.toString();
 
-  for (let i = 0; i < string.length; i++) {
-    if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i' || string.charAt(i) == 'o' || string.charAt(i) == 'u') {
-      vowels += 1;
-    }
+  // for (let i = 0; i < string.length; i++) {
+  //   if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i' || string.charAt(i) == 'o' || string.charAt(i) == 'u') {
+  //     vowels += 1;
+  //   }
+  // } 
+  // return vowels;
+  let count = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  for(let char of data.toLowerCase()) {
+    if (vowels.includes(char)) 
+      count++;
   }
-  return vowels;
+  return count;
+
 };
 
 console.log(numberOfVowels("orange"));
