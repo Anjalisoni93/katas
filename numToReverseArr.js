271d43e7e5e10535b8fe1ebe78a1cfca6c437448
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-22 21:47:59 -0700

    Solve a challenge to reverse the numbers to an array

---
+++ numToReverseArr.js
@@ -0,0 +1,9 @@
+/* Write a function to convert array to reversed array of digits */
+
+function digitize(n) {
+  //code here
+      let arr = String(n).split("").reverse().map(Number);
+      return arr;
+}
+
+console.log(digitize(54321));
