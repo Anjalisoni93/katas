9cf442da4e5354623f32d821d09fdfed40e41192
Anjali Soni <anjalipatadiya@gmail.com>
2023-02-06 22:22:13 -0700

    Add a shorthand solution to jadenCasing problem

--- jadenCasing.js
+++ jadenCasing.js
@@ -1,10 +1,16 @@
 String.prototype.toJadenCase = function () {
-  let arr = this.split(' ');
-  
-  for(let i = 0; i < arr.length; i++) {
+  let arr = this.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
     arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
   }
-  
-  return arr.join(' ');
+
+  return arr.join(" ");
 };
 
+
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map((item) => item[0].toUpperCase() + item.slice(1))
+    .join(" ");
+};
