8e99b41df5b3d26622756d013017377c469844f3
Anjali Soni <anjalipatadiya@gmail.com>
2023-02-08 21:31:28 -0700

    Solve isSquare codewars challenge

---
+++ isSquare.js
@@ -0,0 +1,15 @@
+function isSquare(n) {
+  if (n < 0) {
+    return false;
+  }
+
+  if (Number.isInteger(Math.sqrt(n))) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+console.log(isSquare(16));
+console.log(isSquare(7));
